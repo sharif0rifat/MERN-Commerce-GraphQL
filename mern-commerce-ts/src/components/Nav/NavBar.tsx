58d12131cc96c0002bb152ca9963c3ecbfcc46ed
sharif_rifat <sharif.rifat1986@gmail.com>
2020-04-18 22:23:09 +0600

    nav bar right side added

--- mern-commerce-ts/src/components/Nav/NavBar.tsx
+++ mern-commerce-ts/src/components/Nav/NavBar.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-grid.css'
+import 'bootstrap/dist/css/bootstrap-reboot.css'
+
+
 import './NavBar.css'
 
 class NavBar extends React.Component {
@@ -10,7 +14,7 @@ class NavBar extends React.Component {
                             <div className="row">
                                 <div className="col-lg-2 col-md-2 col-sm-3 col-xs-5"><div className="logo">Logo</div></div>
                                 <div className="col-md-7 col-lg-8 col-sm-5 col-xs-3">
-                                    <nav className="main__menu__nav hidden-xs hidden-sm">
+                                    <nav className="main__menu__nav d-none d-md-block">
                                         <ul className="main__menu">
                                             <li className="drop"><span className="menu-link">HOME</span> </li>
                                             <li className="drop"><span className="menu-link">WOMEN</span> </li>
@@ -20,8 +24,42 @@ class NavBar extends React.Component {
                                             <li className="drop"><span className="menu-link">PAGES</span> </li>
                                         </ul>
                                     </nav>
+                                    <div className="mobile-menu clearfix d-block d-md-none">
+                                    <nav id="mobile_dropdown" >
+                                        <ul>
+                                            <li><span >Home</span></li>
+                                            <li><span >blog</span></li>
+                                            <li><span>pages</span>
+                                                <ul>
+                                                    <li><span >Blog</span></li>
+                                                    <li><span >Blog Details</span></li>
+                                                    <li><span >Cart page</span></li>
+                                                    <li><span >checkout</span></li>
+                                                    <li><span >contact</span></li>
+                                                    <li><span>product grid</span></li>
+                                                    <li><span >product details</span></li>
+                                                    <li><span >wishlist</span></li>
+                                                </ul>
+                                            </li>
+                                            <li><span >contact</span></li>
+                                        </ul>
+                                    </nav></div>                                    
+
                                 </div>
-                                <div className="col-md-3 col-lg-2 col-sm-4 col-xs-4">User</div>
+                                <div className="col-md-3 col-lg-2 col-sm-4 col-xs-4">
+                                <div className="header__right">
+                                    <div className="header__search search search__open">
+                                        <span ><i className="icon-magnifier icons">S</i></span>
+                                    </div>
+                                    <div className="header__account">
+                                        <span ><i className="icon-user icons">U</i></span>
+                                    </div>
+                                    <div className="htc__shopping__cart">
+                                        <span className="cart__menu" >C<i className="icon-handbag icons"></i></span>
+                                        <span ><span className="htc__qua">2</span></span>
+                                    </div>
+                                </div>
+                                </div>
                             </div>
                         </div>
                     </div>
