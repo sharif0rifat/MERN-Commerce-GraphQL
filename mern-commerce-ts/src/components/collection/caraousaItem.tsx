d5c64cdb64abba9acd9acd53746a87a9cab20483
sharif_rifat <sharif.rifat1986@gmail.com>
2020-04-26 14:40:56 +0600

    carousal component complete

--- mern-commerce-ts/src/components/collection/caraousaItem.tsx
+++ mern-commerce-ts/src/components/collection/caraousaItem.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import crsl from "../../assets/css/images/slider/fornt-img/2.png";
-export interface ICarousalItemProps {}
+export interface ICarousalItemProps {
+  chair: string;
+}
 
 export default class CarousalItem extends React.Component<ICarousalItemProps> {
   public render() {
@@ -12,7 +14,7 @@ export default class CarousalItem extends React.Component<ICarousalItemProps> {
               <div className="slide">
                 <div className="slider__inner">
                   <h2>collection 2018</h2>
-                  <h1>NICE CHAIR</h1>
+                  <h1>CHAIR {this.props.chair}</h1>
                   <div className="cr__btn">
                     <a href="#">Shop Now</a>
                   </div>
--- mern-commerce-ts/src/components/collection/collection.tsx
+++ mern-commerce-ts/src/components/collection/collection.tsx
@@ -5,34 +5,69 @@ import "../../assets/css/shortcode/slider.css";
 import "../../assets/css/site/owl.carousel.min.css";
 import CarousalItem from "./caraousaItem";
 import "./collection.css";
-export interface ICollectionProps {}
+import { timingSafeEqual } from "crypto";
+export interface IProps {}
+interface IState {
+  translate?: number;
+}
+export default class Collection extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
 
-export default class Collection extends React.Component<ICollectionProps> {
+    this.state = {
+      translate: 4047,
+    };
+  }
+  moveLeft() {
+    var translate = this.state.translate;
+    if (translate) {
+      translate = translate - 1349;
+      if (translate < 1349) translate = 1;
+    } else translate = 1;
+    this.setState({ ...this.state, translate: translate });
+  }
+  moveRight() {
+    var translate = this.state.translate;
+    if (translate) {
+      translate = translate + 1349;
+      if (translate > 4047) translate = 4047;
+    }
+    this.setState({ ...this.state, translate: translate });
+  }
   public render() {
+    //   const styles = {
+    //     transform: `translate(${x}px, ${y}px)`
+    // }
     return (
       <div className="slider__container slider--one bg__cat--3">
         <div className="slide__container slider__activation__wrap owl-carousel owl-loaded owl-drag">
           <div className="owl-stage-outer">
-            <div className="owl-stage slider-animation">
+            <div
+              className="owl-stage slider-animation"
+              style={{
+                transform:
+                  "translate3d(-" + this.state.translate + "px, 0px, 0px)",
+              }}
+            >
               <div className="owl-item cloned" style={{ width: "1349px" }}>
-                <CarousalItem></CarousalItem>
+                <CarousalItem chair="1"></CarousalItem>
               </div>
               <div className="owl-item cloned" style={{ width: "1349px" }}>
-                <CarousalItem></CarousalItem>
+                <CarousalItem chair="2"></CarousalItem>
               </div>
               <div className="owl-item cloned" style={{ width: "1349px" }}>
-                <CarousalItem></CarousalItem>
+                <CarousalItem chair="3"></CarousalItem>
               </div>
               <div className="owl-item cloned" style={{ width: "1349px" }}>
-                <CarousalItem></CarousalItem>
+                <CarousalItem chair="4"></CarousalItem>
               </div>
             </div>
           </div>
           <div className="owl-nav">
-            <div className="owl-prev">
+            <div className="owl-prev" onClick={this.moveLeft.bind(this)}>
               <i className="fa fa-angle-left"></i>
             </div>
-            <div className="owl-next">
+            <div className="owl-next" onClick={this.moveRight.bind(this)}>
               <i className="fa fa-angle-right"></i>
             </div>
           </div>
