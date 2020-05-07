be53eede5913617d6c319711770ea83a4e21a381
sharif_rifat <sharif.rifat1986@gmail.com>
2020-05-07 13:09:48 +0600

    Product detail page building

--- mern-commerce-ts/src/components/product-detail/product-detail.tsx
+++ mern-commerce-ts/src/components/product-detail/product-detail.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { RouteComponentProps } from "react-router-dom";
-
+import "./product-detail.css";
 interface ProductPrm {
   id: string;
   name: string;
@@ -16,9 +16,26 @@ export default class ProductDetail extends Component<
 
   render() {
     return (
-      <div>
-        Product Detail{this.props.match.params.id}{" "}
-        {this.props.match.params.name}
+      // <div>
+      //   Product Detail{this.props.match.params.id}{" "}
+      //   {this.props.match.params.name}
+      // </div>
+      <div className="container">
+        <div className="row">Bread Crumb</div>
+        <div className="row">
+          <div className="col-md-5 col-lg-5 col-sm-12 col-xs-12">
+            <div className="htc__product__details__tab__content">
+              <div className="product__big__images">
+                <div className="tab-pane  active">
+                  <img src="/img/1.jpg" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-5 col-lg-5 col-sm-12 col-xs-12">
+            Description
+          </div>
+        </div>
       </div>
     );
   }
